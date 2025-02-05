66fbe6652a5199200319fcdebab0a391ea8dc5dd
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-02-05 02:44:43 +0100

    fix: separate npm pkg and server build outputs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "tsup";
 
+// this file is the config for the tsup bundler
+// which build the npm package 
+
 export default defineConfig({
   entry: ["src/index.ts"],
+  outDir: "./lib",
   dts: true,
   format: ["cjs", "iife", "esm"],
   clean: true,
