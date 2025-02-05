db503cfb3613f03634c9bbd611a1ba78c5bb53d4
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-02-05 02:46:52 +0100

    fix: simplify test route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,11 @@
-import { initTRPC } from '@trpc/server';
-import { z } from 'zod';
-
-type User = {
-  id: string;
-  name: string;
-  bio?: string;
-};
-
-const users: Record<string, User> = {};
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
 
 export const t = initTRPC.create();
 export const appRouter = t.router({
-  getUserById: t.procedure.input(z.string()).query((opts) => {
-    return users[opts.input]; // input type is string
+  add: t.procedure.input(z.array(z.number())).query((opts) => {
+    return opts.input.reduce((a, b) => a + b, 0);
   }),
-  createUser: t.procedure
-    .input(
-      z.object({
-        name: z.string().min(3),
-        bio: z.string().max(142).optional(),
-      }),
-    )
-    .mutation((opts) => {
-      const id = Date.now().toString();
-      const user: User = { id, ...opts.input };
-      users[user.id] = user;
-      return user;
-    }),
 });
 
 export type AppRouter = typeof appRouter;
