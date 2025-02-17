0bb4826a9c1fefccaab95bb8df6110a2051db088
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-02-18 00:24:34 +0100

    fix: update trpc to v11

--- backend/tsup.config.ts
+++ backend/tsup.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "tsup";
 
 // this file is the config for the tsup bundler
-// which build the npm package 
+// which build the npm package
 
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "./lib",
   dts: true,
-  format: ["cjs", "iife", "esm"],
+  format: ["cjs", "esm"],
   clean: true,
+  splitting: false,
   sourcemap: true,
 });
+
