486d45aa2a956f1a9e878a030e28d5a903a89bb8
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-04-04 20:06:23 +0200

    ci: change create-db paradigm

--- backend/create-db.ts
+++ backend/create-db.ts
@@ -1,28 +1,32 @@
 import pg from "pg";
 
-const DB_NAME = process.env.DB_NAME;
+async function createDb() {
+  const dbName = process.env.DB_NAME;
 
-const client = new pg.Client({
-  host: process.env.DB_HOST!,
-  port: parseInt(process.env.DB_PORT!),
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASS!,
-  database: undefined,
-});
+  const client = new pg.Client({
+    host: process.env.DB_HOST!,
+    port: parseInt(process.env.DB_PORT!),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASS!,
+    database: undefined,
+  });
 
-try {
-  if (!DB_NAME) throw new Error("No DB_NAME was provided");
+  try {
+    if (!dbName) throw new Error("No DB_NAME was provided");
 
-  await client.connect();
-  const exists = await client
-    .query(`SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'`)
-    .then(({ rowCount }) => rowCount && rowCount === 1)
-    .catch(() => false);
+    await client.connect();
+    const exists = await client
+      .query(`SELECT 1 FROM pg_database WHERE datname='${dbName}'`)
+      .then(({ rowCount }) => rowCount && rowCount === 1)
+      .catch(() => false);
 
-  if (!exists) await client.query(`CREATE DATABASE ${DB_NAME}`)
-  console.log(`Database ${DB_NAME} ${exists ? "already exists" : "created"}`);
-} catch (err) {
-  console.error(err);
-} finally {
-  client.end();
+    if (!exists) await client.query(`CREATE DATABASE ${dbName}`);
+    console.log(`Database ${dbName} ${exists ? "already exists" : "created"}`);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.end();
+  }
 }
+
+createDb();
