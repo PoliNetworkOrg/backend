10a3ca070be31284383d18015ce724f14d4505fd
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2025-04-13 22:29:47 +0200

    feat: migrate fastify to hono (#4)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,61 +1,43 @@
-import {
-  fastifyTRPCPlugin,
-  FastifyTRPCPluginOptions,
-} from "@trpc/server/adapters/fastify";
-import fastify from "fastify";
-import { appRouter, type AppRouter } from "./routers";
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { logger as loggerMiddlware } from "hono/logger";
+import { appRouter } from "./routers";
 import { TRPC_PATH } from "./constants";
-import { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts";
 import { DB, SCHEMA } from "./db";
-import { createTRPCContext } from "./trpc";
 import { env } from "./env";
+import { logger } from "./logger";
+import { trpcServer } from "@hono/trpc-server";
 
-const server = fastify({
-  maxParamLength: 5000,
-  logger: {
-    level: env.LOG_LEVEL,
-  },
-}).withTypeProvider<JsonSchemaToTsProvider>();
+const app = new Hono();
 
 const isDev = env.NODE_ENV === "development";
-server.log.debug(`isDev ${isDev ? "YES" : "NO"}`);
+logger.debug(`isDev ${isDev ? "YES" : "NO"}`);
 
-server.register(fastifyTRPCPlugin, {
-  prefix: TRPC_PATH,
-  trpcOptions: {
+app.use(
+  `${TRPC_PATH}/*`,
+  trpcServer({
     router: appRouter,
-    createContext: createTRPCContext,
-    onError({ path, error }) {
-      // report to error monitoring
-      server.log.error({error}, `Error in tRPC handler on path '${path}':`);
-    },
-  } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"],
-});
+    endpoint: TRPC_PATH,
+    createContext: (_opts, c) => ({ userId: c.req.header("userId") }),
+  }),
+);
 
 if (isDev) {
-  server.addHook("preHandler", async (request) => {
-    // Development-specific logging
-    server.log.debug({
-      path: request.url,
-      method: request.method,
-      body: request.body,
-    });
-  });
+app.use(loggerMiddlware((msg, ...str) => logger.debug(str.length ? {props: str} : undefined, msg)));
 }
 
-server.get("/", (_, res) => res.send("hi"));
-
-const PORT = env.PORT;
+app.get("/", (c) => c.text("hi"));
 
 (async () => {
   try {
-    await server.listen({ port: PORT, host: "0.0.0.0" });
+    serve({ port: env.PORT, hostname: "0.0.0.0", fetch: app.fetch }, (addr) =>
+      logger.info(`Server running on ${addr.address}:${addr.port}`),
+    );
     const q1 = await DB.select().from(SCHEMA.TG.test);
     const q2 = await DB.select().from(SCHEMA.WEB.test);
-    server.log.info({q1, q2}, "db working:",);
-    server.log.info(`listening on port: ${PORT}`);
+    logger.info({ q1, q2 }, "db working:");
   } catch (err) {
-    server.log.error(err);
+    logger.error(err);
     process.exit(1);
   }
 })();
