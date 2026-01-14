708b15baa0e2445ef7cd3f9117828f1c1ed61417
Tommaso Morganti <tommaso.morganti01@gmail.com>
2026-01-14 14:43:00 +0100

    chore: removed legacy code comments

--- backend/src/azure/functions.ts
+++ backend/src/azure/functions.ts
@@ -1,17 +1,9 @@
 import { render, toPlainText } from "@react-email/components"
-// import nodemailer from "nodemailer"
 import MailComposer from "nodemailer/lib/mail-composer"
 import type { JSX } from "react"
 import { env } from "@/env"
 import { logger } from "@/logger"
 import { client } from "./client"
-
-// Create a local transporter for Mailpit
-// const localTransporter = nodemailer.createTransport({
-//   host: "localhost",
-//   port: 1035,
-//   secure: false,
-// })
 
 export async function sendEmail(to: string, subject: string, component: JSX.Element) {
   const html = await render(component)
@@ -37,17 +29,6 @@ export async function sendEmail(to: string, subject: string, component: JSX.Elem
   try {
     await client.api(`/users/${sender}/sendMail`).header("Content-Type", "text/plain").post(base64Encoded)
     logger.info({ subject, to }, "[Azure Graph API] Email sent")
-
-    // localTransporter
-    //   .sendMail({
-    //     from: sender,
-    //     to,
-    //     subject,
-    //     text,
-    //     html,
-    //   })
-    //   .then(() => logger.info({ subject, to }, "[Mailpit] Local email captured"))
-    //   .catch((err) => logger.error({ err }, "[Mailpit] Error while sending email"))
 
     return true
   } catch (error) {
