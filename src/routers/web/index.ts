9d7f62fbeaf98b301c5378896726042806f49355
Bianca <ianoselbiancaroberta02@gmail.com>
2026-06-11 09:49:58 +0200

    feat: remove associations from web router

--- src/routers/web/index.ts
+++ src/routers/web/index.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "@/trpc"
-import associations from "./associations"
 import projects from "./projects"
 
-export const webRouter = createTRPCRouter({ associations, projects })
+export const webRouter = createTRPCRouter({ projects })
