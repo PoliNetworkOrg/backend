7ccd08f0350378dc89d57c8c3e64414804ed77a6
Tommaso Morganti <tommaso.morganti01@gmail.com>
2026-09-07 00:13:13 +0200

    feat: publish npm package

--- release.config.mjs
+++ release.config.mjs
@@ -1,3 +1,32 @@
 export default {
-  branches: [{ name: "main" }, { name: "toto04/semantic-releases", channel: "beta", prerelease: "test" }],
+  branches: [
+    {
+      name: "main",
+    },
+    {
+      name: "toto04/semantic-releases",
+      channel: "beta",
+      prerelease: "test",
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+        pkgRoot: "dist",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failComment: false,
+        failTitle: false,
+        addReleases: "top",
+      },
+    ],
+  ],
 }
